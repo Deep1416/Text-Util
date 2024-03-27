9976773141e96f9647a5ec6f6f5464bcc59dd589
Deependra <deependra186037@gamil.com>
2024-03-27 20:56:43 +0530

    done

--- src/Compments/Footer/Footer.jsx
+++ src/Compments/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div>
       <div className='bg-black text-white p-4 text-center'>
-        <p className='text-lg'>Copyright@2021 | All Rights Reserved | by Deependra Rajput</p>
+        <p className='text-lg'>Copyright@2024 | All Rights Reserved | by Deependra Rajput</p>
       </div>
     </div>
   )
